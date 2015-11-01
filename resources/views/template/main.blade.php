ee79c9b7b228b880257d7c596c01e035f2c9e4df
Shadrak Anthony <shadrak_anthony@hotmail.com>
2015-11-01 23:04:57 +1300

    11:02pm
    =================
    change log
    =================
    *Added jQuery library
    *Added jQuery.js
    *Changes to main blade
    *Changes to sass mixins

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -6,12 +6,30 @@
     <meta name="description" content="logo design art">
     <meta name="author" content="Shadowz Web Development LTD">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="{{asset('css/styles.css')}}">
+    
 </head>
 <body>
-    <head>
-       <img src="" alt="logo">
-        <h1>One Graphic Design</h1>
-    </head>
+    <header>
+        <div class="heading">
+            <img src="" alt="logo">
+            <h1>One Graphic Design</h1>
+            <p id="navBar"><i class="fa fa-bars"></i></p>
+        </div>
+        
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Portfolio</li>
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>Other Art</li>
+            </ul>
+        </nav>
+    </header>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="{{asset('js/jQuery.js')}}"></script>
+    
 </body>
 </html>

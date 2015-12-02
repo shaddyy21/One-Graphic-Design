8a10a9b4390e7fbb7208d4f0320d39654cdea34d
Shadrak Anthony <shadrak_anthony@hotmail.com>
2015-12-02 23:21:01 +1300

    minor changes

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -185,7 +185,6 @@
         </footer>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-   
     <script src="{{asset('js/jQuery.js')}}"></script>
     <script src="{{asset('slick/slick.js')}}"></script>
 </body>
